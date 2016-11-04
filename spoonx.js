a57a380a2cf5d385c85eb74d1cd6cd017116764e
doktordirk <doktordirk@outlook.de>
2016-11-04 11:49:40 +0100

    chore(project): npm scripts, update dep, lint

--- spoonx.js
+++ spoonx.js
@@ -1,3 +1,7 @@
+/************************************************/
+/* spoonx-tools configuration                   */
+/* @see https://github.com/SpoonX/spoonx-tools  */
+/************************************************/
 
 var appRoot = 'src/';
 
@@ -30,8 +34,10 @@ module.exports = {
      * jsResources: [appRoot + 'association-select.js'],
      */
 
-    // other resources that need to get copied keeping their path
-    resources: appRoot + '{**/*.css,**/*.html}',
+    /* other resources that need to get copied keeping their path
+     * resources: appRoot + '{** / *.css,** / *.html}',
+     */
+     resources: appRoot + '{**/*.css,**/*.html}',
   
 
     /* imports that are only used internally, eg 'extend'. no need to d.ts export them
@@ -39,10 +45,19 @@ module.exports = {
      * importsToIgnoreForDts: ['extend'],
      */
 
-    // sort when concating
-    sort: true,
+    /* sort when concating
+     * sort: true,
+     */
+     sort: true,
 
-    // concat js files
-    concat: true
+    /* concat js files
+     * concat: true,
+     */
+     concat: true,
+
+    /* options overwrites for karma. defaults are in ./node_modules/spoonx-tools/karma.conf.js
+     *
+     * karma: {browsers: ['Chrome']}
+     */
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-// @see: http://aurelia.io/hub.html#/doc/article/aurelia/validation/latest/validation-basics/7 
+// @see: http://aurelia.io/hub.html#/doc/article/aurelia/validation/latest/validation-basics/7
 
 export class FormRendererBootstrap {
   render(instruction) {
-    for (let { error, elements } of instruction.unrender) {
+    for (let {error, elements} of instruction.unrender) {
       for (let element of elements) {
         this.remove(element, error);
       }
     }
 
-    for (let { error, elements } of instruction.render) {
+    for (let {error, elements} of instruction.render) {
       for (let element of elements) {
         this.add(element, error);
       }
@@ -17,6 +17,7 @@ export class FormRendererBootstrap {
 
   add(element, error) {
     const formGroup = element.closest('.form-group');
+
     if (!formGroup) {
       return;
     }
@@ -26,6 +27,7 @@ export class FormRendererBootstrap {
 
     // add help-block
     const message = document.createElement('span');
+
     message.className = 'help-block validation-message';
     message.textContent = error.message;
     message.id = `validation-message-${error.id}`;
@@ -34,12 +36,14 @@ export class FormRendererBootstrap {
 
   remove(element, error) {
     const formGroup = element.closest('.form-group');
+
     if (!formGroup) {
       return;
     }
 
     // remove help-block
     const message = formGroup.querySelector(`#validation-message-${error.id}`);
+
     if (message) {
       formGroup.removeChild(message);
 
